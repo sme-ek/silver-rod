d936759f8e0d94dc79fbfd7bc55f9aa89f2fde6d
sme-ek <68247562+sme-ek@users.noreply.github.com>
2021-03-15 17:50:59 -0400

    define ChangeScreen + move function definitions

--- silver_rod.c
+++ silver_rod.c
@@ -1,15 +1,21 @@
 //
 // Created by smeek on 14/03/2021.
 //
+
 #include "raylib.h"
-#include "screens/screens.h"
+#include "C:\Users\18432\Desktop\medoing\silver-rod-git\screens\screens.h"
 #include <stdlib.h>
+
 GameScreen currentScreen = 0;
 
+//global
 const int screenWidth = 1920;
 const int screenHeight = 1080;
 
+//local
+static void ChangeScreen(int screen);
 static void UpdateFrame(void); //updates frame on screen
+
 
 //game loop
 int main(void) {
@@ -23,37 +29,22 @@ int main(void) {
     while (!WindowShouldClose()) {
         UpdateFrame();
     }
-
-//this unloads current screen data before closing application
-    switch (currentScreen) {
-        case RL_LOGO:
-            rlUnloadLogoScreen();
-            break;
-        case DEV_WARNING:
-            UnloadDevScreen();
-            break;
-    }
-    CloseWindow();
-    return 0;
 }
 
-static void ChangeScreen(int screen) {
-        switch (currentScreen) {
-            case RL_LOGO:
-                rlUnloadLogoScreen(); break;
-            case DEV_WARNING:
-                UnloadDevScreen(); break;
-            default: break;
-        }
-        switch (screen) {
-            case RL_LOGO:
-                rlInitLogoScreen(); break;
-            case DEV_WARNING:
-                InitDevScreen(); break;
-            default: break;
-        }
-        currentScreen = screen;
+static void ChangeScreen (int screen){
+    switch (currentScreen) {
+        case RL_LOGO: rlUnloadLogoScreen(); break;
+        case DEV_WARNING: UnloadDevScreen(); break;
+        default: break;
     }
+    switch (screen) {
+        case RL_LOGO: rlInitLogoScreen(); break;
+        case DEV_WARNING: InitDevScreen(); break;
+        default: break;
+    }
+    currentScreen = screen;
+}
+
 static void UpdateFrame(void) {
         switch (currentScreen) {
             case RL_LOGO: {
