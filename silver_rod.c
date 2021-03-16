4381b0b3ee8c883399b4a4fe9eeeac9bad7aa8be
sme-ek <68247562+sme-ek@users.noreply.github.com>
2021-03-16 19:18:18 -0400

    add dev warning screen

--- silver_rod.c
+++ silver_rod.c
@@ -10,43 +10,127 @@ GameScreen currentScreen = 0;
 
 const int screenWidth = 1920;
 const int screenHeight = 1080;
-void UpdateScreen(void); //updates frame on screen
+
+//screen transitions!
+static float transAlpha = 0.0f;
+static bool onTransition = false;
+static bool transFadeOut = false;
+static int transFromScreen = -1;
+static int transToScreen = -1;
+
+static void UpdateScreen();
+
+static void DrawTransition();
 
 int main(void)
 {
-    InitWindow(screenWidth, screenHeight, "silver rod 21.1");
 
-    currentScreen = RL_LOGO;
+//game params
+    InitWindow(screenWidth, screenHeight, "silver rod 21.1");
+    SetTargetFPS(60);
+
+//run through opening sequence
     rlInitLogoScreen();
     InitDevScreen();
 
-    SetTargetFPS(60);
-
     while (!WindowShouldClose()){
-        UpdateScreen();
+        UpdateScreen(); //TODO update to a better command so work with more than just the opening sequence lolz
     }
 }
 
+//fade transition to next screen
+static void TransitionScreen(int screen){
+    onTransition = true;
+    transFadeOut = false;
+    transFromScreen = currentScreen;
+    transToScreen = screen;
+    transAlpha = 0.0f;
+}
+//rectangle transition effect
+static void DrawTransition() {
+    DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), Fade(BLACK, transAlpha));
+}
+
 void ChangeScreen(int screen) {
+    //unload screen first
     switch (currentScreen) {
-        case RL_LOGO: rlUnloadLogoScreen();
-        case DEV_WARNING: UnloadDevScreen();
+        case RL_LOGO: rlUnloadLogoScreen(); break;
+        case DEV_WARNING: UnloadDevScreen(); break;
+        default: break;
     }
+    //switch to next screen
     switch (screen) {
-        case RL_LOGO: rlInitLogoScreen();
-        case DEV_WARNING: DrawDevScreen();
+        case RL_LOGO: rlInitLogoScreen(); break;
+        case DEV_WARNING: DrawDevScreen(); break;
         default: break;
     }
     currentScreen = screen;
 }
 
-void UpdateScreen(void){
-    switch(currentScreen) {
-        case RL_LOGO: {
-            rlUpdateLogoScreen();
-            if (rlFinishLogoScreen()) ChangeScreen(DEV_WARNING);
+void upTransition(void){
+    if (!transFadeOut){
+        transAlpha  += 0.05f;
+        if (transAlpha >= 1.0){
+            transAlpha = 1.0;
+            switch (transFromScreen){
+                case RL_LOGO: rlUnloadLogoScreen(); break;
+                case DEV_WARNING: UnloadDevScreen(); break;
+                default: break;
+            }
+            switch (transToScreen){
+                case RL_LOGO:{
+                    rlInitLogoScreen();
+                    currentScreen = RL_LOGO;
+                } break;
+                case DEV_WARNING:{
+                    InitDevScreen();
+                    currentScreen = DEV_WARNING;
+                } break;
+                default: break;
+            }
+            transFadeOut = true;
         }
-        case DEV_WARNING:
-            break;
+    }
+    else {
+        transAlpha  -= 0.05f;
+        if (transAlpha <= 0){
+            transAlpha = 0;
+            transFadeOut = false;
+            onTransition = false;
+            transFromScreen = -1;
+            transToScreen = -1;
+        }
     }
 }
+
+static void UpdateScreen(void) {
+    if (!onTransition) {
+        switch (currentScreen) {
+            case RL_LOGO: {
+                rlUpdateLogoScreen();
+                if (rlFinishLogoScreen()) TransitionScreen(DEV_WARNING);
+            }
+                break;
+            case DEV_WARNING: {
+                UpdateDevScreen();
+                if (FinishDevScreen() == 1) TransitionScreen(RL_LOGO);
+            } break;
+            default: break;
+        }
+    }
+    else{
+    upTransition();
+    }
+
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+
+    switch(currentScreen){
+        case RL_LOGO: rlDrawLogoScreen(); break;
+        case DEV_WARNING: DrawDevScreen(); break;
+        default: break;
+    }
+    if (onTransition) DrawTransition();
+    EndDrawing();
+}
+
