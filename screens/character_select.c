54b377f08ee1f5611013d9ca5cebe1f61d5ad42c
sme-ek <68247562+sme-ek@users.noreply.github.com>
2021-03-17 19:15:31 -0400

    add character select screen

--- screens/character_select.c
+++ screens/character_select.c
@@ -9,29 +9,51 @@ static int finishScreen;
 static float alpha = 1.0f;
 
 //general variable for character select
-struct Vector2;
-struct Vector3;
+static void fem_player;
+static void masc_player;
+static void selectedPlayer;
 
-static struct Vector2 fem_player; //fem player
-static struct Vector3 masc_player; //masc player
-static Rectangle selectArea; //the area that the player can click to select their character
+//the area that the player can click to select their character
+static Rectangle selectFemArea;
+static Rectangle selectMascArea;
+
+int GetMouseX(void);
+int GetMouseY(void);
 
 void InitCharacterScreen(void){
     framesCounter = 0;
     finishScreen = 0;
+
     //stand in character select radius
-    selectArea = (struct Rectangle){150, 150, 50, 450};
+    selectFemArea = (struct Rectangle){150, 200, 350, 950};
+    selectMascArea = (struct Rectangle){1250, 200, 350, 900};
+}
+
+void SelectCharacter(void){
+    if (GetMouseX() == 150 && GetMouseY() == 200){
+        selectedPlayer == fem_player;
+    }
+    if (GetMouseX() == 1250 && GetMouseY() == 200){
+        selectedPlayer == masc_player;
+    }
 }
 
 void UpdateCharacterScreen(void){
-
+    if (selectedPlayer == fem_player){
+        finishScreen == 1;
+    }
+    if (selectedPlayer == masc_player){
+        finishScreen == 2;
+    }
 }
 
 void DrawCharacterScreen(void){
 DrawRectangle(GetScreenWidth(), GetScreenHeight(), 1920, 1080, Fade(WHITE, alpha));
-DrawCircle(0, 0, 600, Fade(PINK, alpha));
-DrawRectangle (150, 150, 50, 450, PURPLE); //stand in female slot
+DrawCircle(960, 540, 300, Fade(PINK, alpha));
+DrawRectangle (150, 200, 350, 900, PURPLE); //stand in female slot
+DrawRectangle(1450, 200, 350, 900, BLUE); //stand in male slot
 }
+
 void UnloadCharacterScreen(void){
 
 }
