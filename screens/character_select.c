453fa949cae71a17d5e60243ff961c7b51350911
sme-ek <68247562+sme-ek@users.noreply.github.com>
2021-03-17 18:02:26 -0400

    create character_select.c

---
+++ screens/character_select.c
@@ -0,0 +1,25 @@
+//
+// Created by smeek on 17/03/2021.
+//
+
+#include "../include/raylib.h"
+
+static int framesCounter;
+static int finishScreen;
+
+void InitCharacterScreen(void){
+    framesCounter = 0;
+    finishScreen = 0;
+}
+
+void UpdateCharacterScreen(void){
+
+}
+
+void DrawCharacterScreen(void){
+
+}
+
+int FinishCharacterScreen(void){
+    return finishScreen;
+}
